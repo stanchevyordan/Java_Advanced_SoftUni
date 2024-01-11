76b16727903f3d499a90f424aa11b3516fe0ebd3
Yordan Stanchev <yordan.stanchev1998@gmail.com>
2024-01-11 15:36:29 +0200

    Initial commit

---
+++ src/main/java/Stacks_And_Queues/Lab/BrowserHistory.java
@@ -0,0 +1,32 @@
+package Stacks_And_Queues.Lab;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class BrowserHistory {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+        String line = scanner.nextLine();
+
+        String current = "";
+
+        while (!"Home".equals(line)) {
+            if (!"back".equals(line)) {
+                stack.push(line);
+                System.out.println(line);
+            } else {
+                if (!stack.isEmpty()) {
+                    System.out.println("no previous URLs");
+                } else {
+                    stack.pop();
+                    System.out.println(stack.peek());
+                }
+            }
+
+            line = scanner.nextLine();
+        }
+    }
+}
