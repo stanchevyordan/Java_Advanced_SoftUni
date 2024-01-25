8cef6eaab42d208b62d94a746ed776a734e559be
Yordan Stanchev <yordan.stanchev1998@gmail.com>
2024-01-25 12:02:19 +0200

    Create PrintArray_Strings.java

---
+++ src/main/java/Stacks_And_Queues/Additional_Arrays/ArrayPrint/PrintArray_Strings.java
@@ -0,0 +1,10 @@
+package Stacks_And_Queues.Additional_Arrays.ArrayPrint;
+
+public class PrintArray_Strings {
+    public PrintArray_Strings(String[] strings) {
+        for (int j = 0; j < strings.length; j++) {
+            System.out.print(strings[j] + " ");
+        }
+        System.out.println();
+    }
+}
