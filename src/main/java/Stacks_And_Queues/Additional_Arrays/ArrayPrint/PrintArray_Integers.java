165a5eae56dc606b7a80034e218f7166a9a2c033
Yordan Stanchev <yordan.stanchev1998@gmail.com>
2024-01-26 19:55:13 +0200

    Create PrintArray_Integers.java

---
+++ src/main/java/Stacks_And_Queues/Additional_Arrays/ArrayPrint/PrintArray_Integers.java
@@ -0,0 +1,10 @@
+package Stacks_And_Queues.Additional_Arrays.ArrayPrint;
+
+public class PrintArray_Integers {
+    public PrintArray_Integers(int[] arrayName) {
+        for (int j = 0; j < arrayName.length; j++) {
+            System.out.print(arrayName[j] + " ");
+        }
+        System.out.println();
+    }
+}
